2b6a222842c8c07b822293fbcb0a288dc7dc31ee
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-03-11 23:16:43 +0530

    multi theme

--- Router/src/components/Header/Header.jsx
+++ Router/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link,NavLink} from 'react-router-dom'
-import logo from '../../../logo.svg'
+import logo from '../../../public/logo.svg'
 
 export default function Header() {
   return (
