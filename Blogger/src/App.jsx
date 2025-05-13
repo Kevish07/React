0a5af69a748e1085eacf593477b056489860ece1
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-05-13 09:27:11 +0530

    Appwrite and store ready

--- Blogger/src/App.jsx
+++ Blogger/src/App.jsx
@@ -1,9 +1,36 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
-const App = () => (
-  <>
-    <h1>hii</h1>
-  </>
-);
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App;
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div>
+      <Header />
+      <main>
+        Soon : outlet{/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  ) : null;
+}
